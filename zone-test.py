dab8f391665ca24f1213ad10eb2bc01f3e58a38b
yossi <yossi.r@dogtickets.com>
2025-03-18 10:22:20 -0400

    Completed in python

--- zone-test.py
+++ zone-test.py
@@ -21,9 +21,9 @@ LISTINGS_FILE = "listings.json"
 LISTINGS = json.load(open(LISTINGS_FILE, "r")).get("rows", [])
 
 def get_sheet_rows():
-
     print("Getting sheet rows...")
 
+    # Get date from specified sheet ID and range
     result = service.spreadsheets().values().get(
         spreadsheetId=SPREADSHEET_ID,
         range="Zones!A2:F14"
@@ -31,20 +31,48 @@ def get_sheet_rows():
 
     return result.get("values", [])
 
+def update_sheet(values):
+    print("\nUpdating sheet...")
+
+    # Format list of values for Sheets API
+    data = {"values": [[value] for value in values]}
+
+    service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range="Zones!H2:H14",
+        valueInputOption="RAW",
+        body=data
+    ).execute()
+
+    print("Sheet updated successfully!")
+
 def get_avg_price(row):
+    print("\nGetting avg price for row...")
+
     name = row[0]
     sections = row[1].split(",")
     exact_sections = row[2] == "Y"
     count = int(row[5])
 
+    print(f"Name: {name}, Sections: {sections}, Exact Sections: {exact_sections}, Count: {count}")
+
+    print("Finding matching listings for row...")
+
     # Find matching listings for row (zone)
     matching_listings = list(filter(lambda listing: is_matching_listing(listing, sections, exact_sections), LISTINGS))
 
-    # Sort listings by price ascending and slice by count
-    cheapest_listings = matching_listings.sort(key=lambda listing: listing["price"])[:count]
+    # Sort listings by price (low-to-high) and slice by count
+    matching_listings.sort(key=lambda listing: listing["price"])
+    cheapest_listings = matching_listings[:count]
 
-    # Return sum of all prices divided by count
-    return sum(map(lambda listing: listing["price"], cheapest_listings)) / count
+    price_strings = ", ".join([str(listing['price']) for listing in cheapest_listings])
+    print(f"Cheapest listings for row {name}: \n{price_strings}")
+
+    # Return sum of all prices divided by count, rounded down to 2 decimals
+    avg = round(sum(map(lambda listing: listing["price"], cheapest_listings)) / count, 2)
+    print(f"Average: {avg}")
+
+    return avg
 
 def is_matching_listing(listing, sections, exact_sections):
     if exact_sections:
@@ -53,13 +81,14 @@ def is_matching_listing(listing, sections, exact_sections):
         return any(section in listing["section"] for section in sections)
 
 def main():
+    print("Starting main...")
     rows = get_sheet_rows()
     avg_price_values = list(map(get_avg_price, rows))
 
-    print(avg_price_values)
+    # Write the averages generated for each into the H column
+    update_sheet(avg_price_values)
 
-    # update_sheet(avg_price_values)
-
+    print(f"\nAverage Prices: \n{avg_price_values}")
 
 main()
 
